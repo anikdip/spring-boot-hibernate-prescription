060d41cf07431d8289ecbeed107998797a523e4f
Anik Mahamood <anik.dip@gmail.com>
2019-04-30 03:19:19 +0600

    Add Sql File

---
+++ prescription.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 29, 2019 at 11:18 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `prescription`
+--
+CREATE DATABASE IF NOT EXISTS `prescription` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `prescription`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(11),
+(11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+DROP TABLE IF EXISTS `prescription`;
+CREATE TABLE `prescription` (
+                              `id` int(11) NOT NULL,
+                              `date` date NOT NULL,
+                              `name` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+                              `age` int(11) NOT NULL,
+                              `gender` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
+                              `diagnosis` text COLLATE utf8_unicode_ci NOT NULL,
+                              `medicines` text COLLATE utf8_unicode_ci NOT NULL,
+                              `next_visit_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `prescription`
+--
+
+INSERT INTO `prescription` (`id`, `date`, `name`, `age`, `gender`, `diagnosis`, `medicines`, `next_visit_date`) VALUES
+(8, '2019-04-29', 'Anik Mahamood', 30, 'Male', 'Test', 'test', '2019-05-01'),
+(9, '2019-04-30', 'Anik Dip', 25, 'Male', 'Test', 'Test', '2019-05-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+                      `id` bigint(20) NOT NULL,
+                      `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(2, 'ROLE_USER'),
+(4, 'ROLE_USER'),
+(6, 'ROLE_USER'),
+(10, 'ROLE_USER');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+                      `id` bigint(20) NOT NULL,
+                      `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+                      `first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+                      `last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+                      `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `first_name`, `last_name`, `password`) VALUES
+(1, 'anik.dip@gmail.com', 'Anik', 'Mahamood', '$2a$10$R0V3Vu80CvFJRljR4tSGW.rKVBkwzj.4ee3fRPBXEcsoTZq21qybW'),
+(3, 'dipa@gmail.com', 'Anik', 'Mahamood', '$2a$10$cczW.2Nkg41rTEMXWhDFEOUr.yN1wkqjSH/gBJB7UDnKuaMA64UuW');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `users_roles`;
+CREATE TABLE `users_roles` (
+                             `user_id` bigint(20) NOT NULL,
+                             `role_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES
+(1, 2),
+(3, 10);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UKob8kqyqqgmefl0aco34akdtpe` (`email`);
+
+--
+-- Indexes for table `users_roles`
+--
+ALTER TABLE `users_roles`
+  ADD KEY `FKt4v0rrweyk393bdgt107vdx0x` (`role_id`),
+  ADD KEY `FKgd3iendaoyh04b95ykqise6qh` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `prescription`
+--
+ALTER TABLE `prescription`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_roles`
+--
+ALTER TABLE `users_roles`
+  ADD CONSTRAINT `FKgd3iendaoyh04b95ykqise6qh` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FKt4v0rrweyk393bdgt107vdx0x` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+COMMIT;
